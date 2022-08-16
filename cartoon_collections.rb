aeafcc0fd49d16097f0f2a53f440ea11bae790f6
Pascal Odudigbo <odudigbopascal@gmail.com>
2022-08-17 02:06:56 +0300

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,36 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array_of_dwarves)
   # Your code here
+  array_of_dwarves.each.with_index do |dwarf, index|
+    puts "#{index + 1}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array_of_arguments)
   # Your code here
+  array_of_arguments.map do |argument|
+    argument.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array_of_calls)
   # Your code here
+  result = array_of_calls.find do |call|
+    call.length > 4
+  end 
+  if result == nil
+    false
+  else
+    true
+  end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array_of_strings)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array_of_strings.each do |string|
+    if cheese_types.include?(string) == true
+      return string
+    end
+  end
+  nil
 end
